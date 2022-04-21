bb883eee7d9ca23358251827af1ec553afecbecc
7132 <17630307132@163.com>
2022-04-21 22:03:56 +0800

    项目提交

---
+++ src/main/java/com/example/demo/controller/hello.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class hello {
+    @RequestMapping("/xin")
+    public String hello(){
+
+        return "hello  miss 7132";
+    }
+}
